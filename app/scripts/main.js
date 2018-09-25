8e953e1148ab316425a348f7830b22094e0fb421
bakhaa <bakhaa101@gmail.com>
2018-09-25 23:06:54 +0500

    add sections

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,22 @@
-console.log('I`m ready');
+$(document).ready(function () {
+  $('.owl-carousel').owlCarousel({
+    loop: true,
+    margin: 50,
+    autoplay: true,
+    responsive: {
+      0: {
+        items: 1,
+        nav: true
+      },
+      700: {
+        items: 2,
+        nav: false
+      },
+      1000: {
+        items: 3,
+        nav: true,
+        loop: false
+      }
+    }
+  });
+});
